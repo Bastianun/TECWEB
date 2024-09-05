c3e60ba00361f09f2c2bc4f3d06018e9c3cc2b8d
Bastian Zarate <bastian.zarate.ayala@cftmail.cl>
2024-09-05 11:02:02 -0400

    Mi primer proyecto en GitHub

---
+++ web/javascript.js
@@ -0,0 +1,10 @@
+function validar(){
+    if (document.getElementById("pass").value != document.getElementById("rpass").value){
+        alert("Contraseña no iguales");
+    }
+    else{
+        alert("Listo")
+    }
+}
+
+
